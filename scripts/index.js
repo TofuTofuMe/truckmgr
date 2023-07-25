6ce2dcca63e7bad59b5fbfc5dd0680961bd1d9e2
TofuTofuMe <santoschristopheredison@protonmail.com>
2023-07-25 14:51:06 +0800

    Refactor table data retrieval

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,47 @@ async function handleSelectorChange(selectorId, formFieldIds, referenceFieldId) 
         }
     });
 }
+
+async function retrieveTableData(tableId) {
+    var dataRow = 0;
+    const table = document.getElementById(tableId);
+    document.addEventListener('DOMContentLoaded', () => {
+        switch (table.id) {
+            case 'trucks':
+                request = new Request('/trucks/list_trucks', {method: 'GET'});
+                break;
+            case 'trips':
+                request = new Request('/trips/list_trips', {method: 'GET'});
+                break;
+            case 'maintenance':
+                request = new Request('/maintenance/list_maintenance', {method: 'GET'});
+                break;
+            default:
+                {}
+        }
+        tableBody = document.querySelector('#table-body');
+        fetch(request).then((response) => {
+            response.json().then(responseData => {
+
+                if (responseData.length === 0) {
+                    responseData.push({
+                        blankmsg: 'No data available'
+                    })
+                }
+
+                responseData.forEach(entry => {
+                    const tableRow = document.createElement('tr');
+                    for (const data in entry) {
+                        const tableData = document.createElement('td');
+                        tableData.setAttribute('id', `${data}-${dataRow}`);
+                        tableData.textContent = entry[data];
+                        tableRow.appendChild(tableData);
+                    }
+                    tableRow.setAttribute('id', `entry-${dataRow}`);
+                    tableBody.appendChild(tableRow);
+                    dataRow++;
+                })
+            })
+        });
+    })
+}
