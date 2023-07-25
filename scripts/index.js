8fda0cafb961d20f2e9a04996edf97f3b82b0eed
TofuTofuMe <santoschristopheredison@protonmail.com>
2023-07-25 13:39:19 +0800

    Create selector change handling function

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+async function selectorChangeHandle(selectorId, formFieldIds, referenceFieldId) {
+    const selector = document.getElementById(selectorId);
+
+    const referenceField = document.getElementById(referenceFieldId);
+    const dropButton = document.getElementById('dropButton');
+
+    selector.addEventListener('change', () => {
+        const selected = selector.value;
+
+        formFieldIds.forEach(fieldId => {
+            const formField = document.getElementById(fieldId);
+
+            if (selected === 'new') {
+                formField.value = '';
+            } else {
+                formField.value = document.getElementById(`${fieldId}-${selected - 1}`).innerHTML;
+            }
+        });
+
+        if (selector.value !== 'new') {
+            referenceField.readOnly = true;
+            dropButton.disabled = false;
+        } else {
+            referenceField.readOnly = false;
+            dropButton.disabled = true;
+        }
+    });
+}
